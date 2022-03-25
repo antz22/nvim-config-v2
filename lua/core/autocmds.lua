576165bb1452eb5659505696acb3b86e4264df87
bobycat22 <anthonyznj@gmail.com>
2022-03-25 00:09:17 -0400

    add cpp compile keybindings

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -3,8 +3,8 @@ local settings=require("core.utils").load_config().options.nvChad
 -- vim.cmd [[ autocmd BufEnter * if &buftype != "terminal" | lcd %:p:h | endif ]]
 
 -- Use relative & absolute line numbers in 'n' & 'i' modes respectively
--- vim.cmd[[ au InsertEnter * set norelativenumber ]]
--- vim.cmd[[ au InsertLeave * set relativenumber ]]
+vim.cmd[[ au InsertEnter * set norelativenumber ]]
+vim.cmd[[ au InsertLeave * set relativenumber ]]
 
 -- Don't show any numbers inside terminals
 if not settings.terminal_numbers then
@@ -17,4 +17,6 @@ vim.cmd [[ autocmd BufEnter,BufRead,BufWinEnter,FileType,WinEnter * lua require(
 -- Open a file from its last left off position
 -- vim.cmd [[ au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif ]]
 -- File extension specific tabbing
--- vim.cmd [[ autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+vim.cmd [[ autocmd Filetype cpp setlocal expandtab tabstop=4 shiftwidth=4 softtabstop=4 ]]
+
+
