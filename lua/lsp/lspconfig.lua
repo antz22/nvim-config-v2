68e5b76a88548503f71af45fb5c972e3d37bd298
bobycat22 <anthonyznj@gmail.com>
2022-03-24 23:39:21 -0400

    initial commit

---
+++ lua/lsp/lspconfig.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+M.setup_lsp = function(attach, capabilities)
+   local lspconfig = require "lspconfig"
+
+   -- lspservers with default config
+   local servers = { "html", "cssls", "bashls", "clangd", "emmet_ls", "dartls", "pyright", "eslint", "tsserver" }
+
+   for _, lsp in ipairs(servers) do
+      lspconfig[lsp].setup {
+         on_attach = attach,
+         capabilities = capabilities,
+         flags = {
+            debounce_text_changes = 150,
+         },
+      }
+   end
+end
+
+return M
